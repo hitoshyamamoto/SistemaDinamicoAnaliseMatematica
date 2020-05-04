870a9a068cb964cce704d5135ed7b530bb774e72
Hitoshi Yamamoto <60470969+hitoshyamamoto@users.noreply.github.com>
2020-05-04 16:40:25 -0300

    Teste Funcional 1.0.0
    Versão final funcional, no entanto para funções mais complexas há resultados errados.

--- transformadaLaPlace.m
+++ transformadaLaPlace.m
@@ -4,17 +4,17 @@ function calcularTransformadaLaplace
   
   if(tipoOperacao == 1)
     disp("Coloque os valores no numerador da função:\n")
-    disp("Por exemplo:\n(3*t + 2*exp(3*t))\nResposta: 2/(s-3) + 3/s^2\n")
+    disp("[ Por exemplo: ]\n(3*t + 2*exp(3*t))\n[ Resposta: ]\n2/(s-3) + 3/s^2\n")
     syms F f s t
     f = input("Sua função:\n")
-    F = laplace(f)
+    F = laplace(f,t,s)
   endif
   if(tipoOperacao == 2)
     disp("Coloque os valores no numerador da função:\n")
-    disp("Por exemplo:\n1/((s+3)*(s-2))\nResposta: exp(2*t)/5 - 1/(5*exp(3*t))\n")
+    disp("[ Por exemplo: ]\n1/((s+3)*(s-2))\n[ Resposta: ] exp(2*t)/5 - 1/(5*exp(3*t))\n")
     syms F f s t
     F = input("Sua funcao:\n")
-    f = ilaplace(F)
+    f = ilaplace(F,s,t)
   endif
 
 endfunction
